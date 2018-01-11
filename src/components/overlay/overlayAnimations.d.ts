966477c53c42597f3b668d71e1af380187fc11c4
Amir Zuker <a.zuker@gmail.com>
2018-01-11 18:11:08 +0200

    update

--- src/components/overlay/overlayAnimations.d.ts
+++
@@ -1,4 +0,0 @@
-import { AnimationEntryMetadata } from '@angular/core';
-export declare const slideTopDownAnimation: AnimationEntryMetadata;
-export declare const slideLeftToRightAnimation: AnimationEntryMetadata;
-export declare const fadeAnimation: AnimationEntryMetadata;
--- zuker-ngscale-ngcomponentlib.d.ts
+++ zuker-ngscale-ngcomponentlib.d.ts
@@ -2,5 +2,4 @@
  * Generated bundle index. Do not edit.
  */
 export * from './index';
-export { slideLeftToRightAnimation as ɵb, slideTopDownAnimation as ɵa } from './src/components/overlay/overlayAnimations';
-export { TabsPresenterDirective as ɵc } from './src/components/tabs/tabs-presenter.directive';
+export { TabsPresenterDirective as ɵa } from './src/components/tabs/tabs-presenter.directive';
